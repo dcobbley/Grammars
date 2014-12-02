e262325fe0ace884e13df3ac260b3ad3900a61c2
dcobbley <djcobbles@gmail.com>
2014-12-01 16:52:42 -0800

    began algorithm layout.
    Wrote up simple push/pop algorithm

--- GrammarAnalyzer/GrammarAnalyzer/Form1.Designer.cs
+++ GrammarAnalyzer/GrammarAnalyzer/Form1.Designer.cs
@@ -28,21 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tb_StringToTest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tb_StringToTest
+            // 
+            this.tb_StringToTest.Location = new System.Drawing.Point(48, 53);
+            this.tb_StringToTest.Name = "tb_StringToTest";
+            this.tb_StringToTest.Size = new System.Drawing.Size(144, 20);
+            this.tb_StringToTest.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter a string here.";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(48, 91);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(448, 324);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_StringToTest);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_StringToTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_submit;
     }
 }
 
--- GrammarAnalyzer/GrammarAnalyzer/Form1.cs
+++ GrammarAnalyzer/GrammarAnalyzer/Form1.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+using System.IO;
 
 namespace GrammarAnalyzer
 {
     public partial class Form1 : Form
     {
+        string inputBuffer = "";
+        string[] inputGrammar=null;
+        Stack myStack = null;
+        int count;
+        bool acceptFlag = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +27,39 @@ namespace GrammarAnalyzer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            myStack= new Stack();
+        }
 
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            inputBuffer = tb_StringToTest.Text.ToString();
+        }
+
+        private void getGrammar()
+        {
+            string path = Directory.GetCurrentDirectory();
+            inputGrammar = System.IO.File.ReadAllLines(path = @"/sampleInput.txt");
+        }
+
+        private void loadStack(int i)
+        {
+            getGrammar();
+            //push start state into stack.
+            myStack.Push(inputGrammar[i].[0]);
+        }
+
+        private void popStack()
+        {
+            //pop an element
+
+            //if elemenet is a variable, look at next element of inputBuffer, determine which rule to use.
+            //push right hand side of rule onto stack, else reject if no match.
+
+            //If the element is a terminal, check it matches next character in input and remove both. else reject.
+
+            //if stack is empty and inputbuffer is not, reject.
+
+            //if both stack and input buffer empty, accept.
         }
     }
 }
